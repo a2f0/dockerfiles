052c680932b9839153c159b02cace8eccb8cd8d2
Dan Sullivan <dansullivan@gmail.com>
2016-12-31 21:23:34 -0600

    working in passing of environment variables for s3fs

--- ubuntu-dokuwiki/init.sh
+++ ubuntu-dokuwiki/init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-docker build -t ubuntu-dokuwiki .
+#docker build -t ubuntu-dokuwiki .
 #docker run -p 80:80 -d ubuntu-dokuwiki
-docker-compose up
+#docker-compose up
+source init-env.sh
+docker run -t -i -e S3FS_USER=$S3FS_USER -e S3FS_PASS=$S3FS_PASS ubuntu-apache
