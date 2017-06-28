34dab1cfe568707d7bbde8015b8feecafcf68edb
Dan Sullivan <dansullivan@gmail.com>
2017-06-28 11:45:04 -0500

    pass in variables with rocker run for ubuntu-dokuwiki image

--- ubuntu-dokuwiki/init.sh
+++ ubuntu-dokuwiki/init.sh
@@ -3,4 +3,10 @@
 #docker run -p 80:80 -d ubuntu-dokuwiki
 #docker-compose up
 source init-env.sh
-docker run --cap-add SYS_ADMIN --device /dev/fuse -t -i -e AWSACCESSKEYID=$S3FS_USER -e AWSSECRETACCESSKEY=$S3FS_PASS ubuntu-dokuwiki
+docker run --cap-add SYS_ADMIN --device /dev/fuse -t -i \
+  -e AWSACCESSKEYID=$S3FS_USER \
+  -e AWSSECRETACCESSKEY=$S3FS_PASS \
+  -h dokuwiki \
+  -p 80:80 \
+  --name=dokuwiki \
+  ubuntu-dokuwiki
