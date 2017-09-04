77ffd3192eb29b1c45d74803b34952c8a6c7c9b7
Dan Sullivan <dansullivan@gmail.com>
2017-09-04 12:09:48 -0500

    start_over -> implode

--- implode.sh
+++ implode.sh
@@ -4,6 +4,4 @@ docker stop $(docker ps -a -q)
 # Delete all containers
 docker rm $(docker ps -a -q)
 # Delete all images
-docker rmi $(docker images -q)
-# Rebuild them
-./build_me.sh
+docker rmi -f $(docker images -q)
